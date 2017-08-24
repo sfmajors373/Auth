f9af57f9b52e64450b1e521a90073c3cf4c41fee
sfmajors373 <sfmajors373@gmail.com>
2017-08-24 15:46:53 -0400

    UserSchema created, POST /users throws 500 error

--- src/server.js
+++ src/server.js
@@ -1,15 +1,20 @@
 const bodyParser = require('body-parser');
 const express = require('express');
 const session = require('express-session');
+const User = require('./user');
+const mongoose = require('mongoose');
 
 const STATUS_USER_ERROR = 422;
+const STATUS_SERVER_ERROR = 500;
 const BCRYPT_COST = 11;
 
 const server = express();
 // to enable parsing of json bodies for post requests
 server.use(bodyParser.json());
 server.use(session({
-  secret: 'e5SPiqsEtjexkTj3Xqovsjzq8ovjfgVDFMfUzSmJO21dtXs4re'
+  secret: 'e5SPiqsEtjexkTj3Xqovsjzq8ovjfgVDFMfUzSmJO21dtXs4re',
+  resave: true,
+  saveUninitialized: false
 }));
 
 /* Sends the given err, a string or an object, to the client. Sets the status
@@ -25,10 +30,30 @@ const sendUserError = (err, res) => {
 
 // TODO: implement routes
 
-// TODO: add local middleware to this route to ensure the user is logged in
-server.get('/me', (req, res) => {
-  // Do NOT modify this route handler in any way.
-  res.json(req.user);
+// POST /users
+server.post('/users', (req, res) => {
+  const { username, password } = req.body;
+  if (!username || !password) {
+    // res.status(STATUS_USER_ERROR);
+    // res.json({ error: 'Please provide a username and password' });
+    sendUserError('Please provide a username and password', res);
+    return;
+  }
+  const user = new User({ username, password });
+  user.save((err) => {
+    if (err) {
+      res.status(STATUS_SERVER_ERROR);
+      res.json(err);
+    } else {
+      res.json(user);
+    }
+  });
 });
+
+// TODO: add local middleware to this route to ensure the user is logged in
+// server.get('/me', (req, res) => {
+  // Do NOT modify this route handler in any way.
+//   res.json(req.user);
+// });
 
 module.exports = { server };
--- src/user.js
+++ src/user.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const SALT_WORK_FACTOR = 11;
 
 // Clear out mongoose's model cache to allow --watch to work for tests:
 // https://github.com/Automattic/mongoose/issues/1251
@@ -10,6 +12,47 @@ mongoose.connect('mongodb://localhost/users', { useMongoClient: true });
 
 const UserSchema = new mongoose.Schema({
   // TODO: fill in this schema
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  passwordHash: {
+    type: String,
+    required: true,
+  }
 });
+
+UserSchema.pre('save', function(next) {
+  const user = this;
+  // only hash the password if it has been modified (or is new)
+  if (!user.isModified('password')) {
+    return;
+    next();
+  }
+  bcrypt.genSalt(SALT_WORK_FACTOR, ((err, salt) => {
+    if (err) {
+      return;
+      next(err);
+    }
+    bcrypt.hash(user.password, salt, ((err, hash) => {
+      if (err) {
+        return;
+        next(err);
+      }
+      user.password = hash;
+      next();
+    }));
+  }));
+});
+
+UserSchema.methods.comparePassword = function(candidatePassword, cb) {
+  bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
+    if (err) {
+      return cb(err);
+    }
+    cb(null, isMatch);
+  });
+};
 
 module.exports = mongoose.model('User', UserSchema);
