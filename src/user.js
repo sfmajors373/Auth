a2e789907974429ac1c2e6a88d9ed801c6d3a9a8
sfmajors373 <sfmajors373@gmail.com>
2017-08-24 16:47:12 -0400

    Corrected linting errors in user.js

--- src/user.js
+++ src/user.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
+
 const bcrypt = require('bcrypt');
+
 const SALT_WORK_FACTOR = 11;
 
 // Clear out mongoose's model cache to allow --watch to work for tests:
@@ -23,36 +25,36 @@ const UserSchema = new mongoose.Schema({
   }
 });
 
-UserSchema.pre('save', function(next) {
+UserSchema.pre('save', ((next) => {
   const user = this;
   // only hash the password if it has been modified (or is new)
   if (!user.isModified('password')) {
-    return;
+    // return;
     next();
   }
   bcrypt.genSalt(SALT_WORK_FACTOR, ((err, salt) => {
     if (err) {
-      return;
+      // return;
       next(err);
     }
-    bcrypt.hash(user.password, salt, ((err, hash) => {
-      if (err) {
-        return;
-        next(err);
+    bcrypt.hash(user.password, salt, ((error, hash) => {
+      if (error) {
+        // return;
+        next(error);
       }
       user.password = hash;
       next();
     }));
   }));
-});
+}));
 
-UserSchema.methods.comparePassword = function(candidatePassword, cb) {
-  bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
+UserSchema.methods.comparePassword = ((candidatePassword, cb) => {
+  bcrypt.compare(candidatePassword, this.password, ((err, isMatch) => {
     if (err) {
       return cb(err);
     }
     cb(null, isMatch);
-  });
-};
+  }));
+});
 
 module.exports = mongoose.model('User', UserSchema);
